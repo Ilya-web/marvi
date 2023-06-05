aa0403bab65ce635b19b50793d3722e62e08eb70
Svitlana Apolokhova <37361118+cweet-dreams@users.noreply.github.com>
2023-06-05 13:15:32 +0300

    fixed

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -342,23 +342,23 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     //--------------------------------------------------------------------
-    const imageParallax = gsap.utils.toArray('.imageParallax img');
-
-    if(imageParallax.length !== 0) {
-      const parentImageParallax = gsap.timeline({
-        scrollTrigger: {
-          trigger: ".parentImageParallax",
-          scrub: true,
-          start: "top top",
-        },
-      });
-      parentImageParallax.to(
-        imageParallax,
-        {
-          y: 300,
-        }
-      );
-    }
+    // const imageParallax = gsap.utils.toArray('.imageParallax img');
+    //
+    // if(imageParallax.length !== 0) {
+    //   const parentImageParallax = gsap.timeline({
+    //     scrollTrigger: {
+    //       trigger: ".parentImageParallax",
+    //       scrub: true,
+    //       start: "top top",
+    //     },
+    //   });
+    //   parentImageParallax.to(
+    //     imageParallax,
+    //     {
+    //       y: 300,
+    //     }
+    //   );
+    // }
 
     //------------------------------------------------------------------
     const smartMARVIItemAnimate = gsap.utils.toArray('.smartMARVI-item--animate');
@@ -435,30 +435,30 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
 
-    //counter--------------------------------------------------
-    const counter = document.querySelectorAll('.counter');
-
-    function animateValue(obj, start, end, duration) {
-      let startTimestamp = null;
-      const step = (timestamp) => {
-        if (!startTimestamp) startTimestamp = timestamp;
-        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-        obj.innerHTML = Math.floor(progress * (end - start) + start);
-        if (progress < 1) {
-          window.requestAnimationFrame(step);
-        }
-      };
-      window.requestAnimationFrame(step);
-    }
-    counter.forEach(item => {
-      const count = item.getAttribute('data-count')
-      animateValue(item, 0, count, 1500);
-    })
-
     return () => {
       lenis.destroy();
     };
   });
+
+  //counter--------------------------------------------------
+  const counter = document.querySelectorAll('.counter');
+
+  function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj.innerHTML = Math.floor(progress * (end - start) + start);
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+  counter.forEach(item => {
+    const count = item.getAttribute('data-count')
+    animateValue(item, 0, count, 1500);
+  })
 
 
   //more-btn----------------------------------------------------
